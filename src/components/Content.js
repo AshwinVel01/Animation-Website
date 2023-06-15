bebd23ca1ae64c00cc8711d4d0d60e385762f481
AshwinVel01 <Ashwin.Velusamy01@outlook.com>
2023-06-15 21:57:20 +0530

    Updated Content component with new features

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,41 +1,41 @@
-import React from "react"
-import './Content.scss'
-import {Route, Routes, useLocation} from "react-router-dom";
-import { AnimatePresence } from 'framer-motion';
+import React from "react";
+import "./Content.scss";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
-import Description from "../pages/Description"
-import Range from "../pages/Range"
-import Diet from "../pages/Diet"
-import LifeHistory from '../pages/LifeHistory';
-import Conservation from '../pages/Conservation';
+import Description from "../pages/Description";
+import Range from "../pages/Range";
+import Diet from "../pages/Diet";
+import LifeHistory from "../pages/LifeHistory";
+import Conservation from "../pages/Conservation";
 import About from "../pages/About";
 
 const routes = [
-    { path: "/", name: "Home", Component: Description },
-    {path: "/", name: "Description", Component: Description},
-    { path: "/range", name: "Range", Component: Range },
-    { path: "/diet", name: "Diet", Component: Diet },
-    { path: "/life-history", name: "LifeHistory", Component: LifeHistory },
-    { path: "/conservation", name: "Conservation", Component: Conservation },
-    { path: "/about", name: "About", Component: About }
+  { path: "/", name: "Home", Component: Description },
+  { path: "/description", name: "Description", Component: Description },
+  { path: "/range", name: "Range", Component: Range },
+  { path: "/diet", name: "Diet", Component: Diet },
+  { path: "/life-history", name: "LifeHistory", Component: LifeHistory },
+  { path: "/conservation", name: "Conservation", Component: Conservation },
+  { path: "/about", name: "About", Component: About },
+  { path: "*", name: "Not Found", Component: Description }, // Default route for unmatched URLs
 ];
 
-const routeComponents = routes.map(({path, Component}) =>
-    <Route path={path} element={<Component/>}/>
-);
+const routeComponents = routes.map(({ path, Component }) => (
+  <Route key={path} path={path} element={<Component />} />
+));
 
 function Content() {
-    const location = useLocation();
+  const location = useLocation();
 
-    return(
-        <div className="content">
-            <AnimatePresence exitBeforeEnter>
-                <Routes key={location.pathname} location={location}>
-                    {routeComponents}
-                </Routes>
-            </AnimatePresence>
-        </div>
-    );
+  return (
+    <div className="content">
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location}>{routeComponents}</Routes>
+      </AnimatePresence>
+    </div>
+  );
 }
 
 export default Content;
+
